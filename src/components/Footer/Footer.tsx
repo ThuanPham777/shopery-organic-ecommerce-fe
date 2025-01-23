42bc4925b2689294e5a0cb76c0ce119d3ae6c5de
ThuanPham777 <phamchithuan777@gmail.com>
2025-01-23 21:21:55 +0700

    setup basic project organization

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>&copy; Shopery Organic 2025</p>
+            </footer>
+        </div>
+    )
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+    <div>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/products">Products</a></li>
+          </ul>
+        </nav>
+    </div>
+    )
+}
